060e9b703687a0290163ba37a31486b7d26ec8be
diralfeastarea <diralfeastarea@gmail.com>
2018-03-22 12:54:24 +0000

    background

--- gdx-game/src/com/eastarea/mygame2/game/GameBackground.java
+++ gdx-game/src/com/eastarea/mygame2/game/GameBackground.java
@@ -9,13 +9,22 @@ import com.badlogic.gdx.graphics.glutils.*;
 public class GameBackground implements IRenderable
 {
     BrutalGame game;
-    public TextureRegion backgroundTexture;
+    public TextureRegion backgroundTexture0;
+	public TextureRegion backgroundTexture1;
+	public TextureRegion backgroundTexture2;
+	public TextureRegion backgroundTexture3;
     
     public GameBackground(BrutalGame game)
     {
         this.game = game;
-        Texture texture = new Texture(Gdx.files.internal("background.jpg"));
-        backgroundTexture = new TextureRegion(texture, 0, 0, 563, 563);
+        Texture texture = new Texture(Gdx.files.internal("background0.png"));
+        backgroundTexture0 = new TextureRegion(texture, 0, 0, 563, 563);
+		texture = new Texture(Gdx.files.internal("background1.png"));
+        backgroundTexture1= new TextureRegion(texture, 0, 0, 563, 563);
+		texture = new Texture(Gdx.files.internal("background2.png"));
+        backgroundTexture2 = new TextureRegion(texture, 0, 0, 563, 563);
+		texture = new Texture(Gdx.files.internal("background3.png"));
+        backgroundTexture3 = new TextureRegion(texture, 0, 0, 563, 563);
         
     }
     
@@ -31,15 +40,25 @@ public class GameBackground implements IRenderable
         batch.begin();
         batch.disableBlending();
         
-        float gcx = game.session.guitarCube.position.x;
-        float sx = (((int)(gcx/1600))-1)*800 + gcx/2;
+        renderBack(batch, backgroundTexture0, 0.0625f);
+        
+        batch.enableBlending();
+		
+		renderBack(batch, backgroundTexture1, 0.125f);
+        renderBack(batch, backgroundTexture2, 0.25f);
+        renderBack(batch, backgroundTexture3, 0.5f);
+        
+        batch.end();
+    }
+	
+	void renderBack(SpriteBatch batch, TextureRegion backgroundTexture, float speed)
+	{
+		float gcx = game.session.guitarCube.position.x;
+        float sx = (((int)(gcx*speed/800))-1)*800 + (gcx - gcx*speed);
         for (int i = 0; i < 3; i++)
         {
             float x = i * 800 + sx;
             batch.draw(backgroundTexture, x, 0, 800, 800);
         }
-        
-        batch.enableBlending();
-        batch.end();
-    }
+	}
 }
